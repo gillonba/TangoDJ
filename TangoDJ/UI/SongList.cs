db2e7cf011840e37127be87a52b55cc2c637c297
unknown-user <unknown-user@192.168.2.6>
2013-07-30 01:27:02 -0400

    New SongList element for listing all songs in the library

---
+++ TangoDJ/UI/SongList.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace TangoDJ
+{
+	public class SongList : Gtk.Bin 
+	{
+		public SongList ()
+		{
+		}
+
+		public Gtk.Table BuildSongList(){
+			Gtk.Table t = new Gtk.Table(1,2,false);
+
+			throw new NotImplementedException();
+
+		}
+
+		public void SetSongs(SongInfo[] sia){
+			throw new NotImplementedException();
+		}
+	}
+}
+
