42d594d7fe6e82d96b8d5f53804a2bf576b27c13
barron <unknown-user@192.168.2.11>
2013-08-04 23:42:58 -0400

    Added SongInfoExtended control.  May not use or implement this for a bit

---
+++ TangoDJ/UI/SongInfoExtended.cs
@@ -0,0 +1,24 @@
+using Gtk;
+using System;
+
+namespace TangoDJ
+{
+	public class SongInfoExtended :Gtk.Bin 
+	{
+		public SongInfoExtended ()
+		{
+		}
+
+		public Table BuildSongInfoExtended ()
+		{
+			Gtk.Table t = new Gtk.Table(1,1,false);
+
+			throw new NotImplementedException();
+		}
+
+		public void SetSongInfo(TangoDJ.Library.SongInfo si){
+
+		}
+	}
+}
+
